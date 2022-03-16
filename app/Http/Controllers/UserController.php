20d78a14b06b46afd35017434c42ea54fa1842ba
Xistech Tiago <tiagoxistech@gmail.com>
2022-03-15 21:30:15 -0300

    Criação de rotas e acesso ao banco de dados

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    { 
+      //acesso a camada model
+       $users = User::all();
+      
+       //debug
+      // dd($users);
+    
+       return view('users',[
+           'users' => $users
+       ]); 
+    }
+}
